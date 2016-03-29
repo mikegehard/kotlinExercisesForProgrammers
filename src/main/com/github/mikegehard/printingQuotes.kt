22d65fa1293d0d0368a6545c8c454ee930fcc49d
Mike Gehard <mike.gehard@gmail.com>
2016-03-29 06:25:54 -0600

    Use a data class instead of a pair."

--- src/main/com/github/mikegehard/printingQuotes.kt
+++ src/main/com/github/mikegehard/printingQuotes.kt
@@ -1,9 +1,11 @@
 package com.github.mikegehard
 
+data class Quote(val person: String, val text: String)
+
 fun main(args: Array<String>) {
     val quotes = listOf(
-            Pair("Ob-Wan Kenobi", "These aren't the droids you're looking for.")
+            Quote("Ob-Wan Kenobi", "These aren't the droids you're looking for.")
     )
 
-    quotes.forEach { println("${it.first} says, \"${it.second}\"") }
+    quotes.forEach { println("${it.person} says, \"${it.text}\"") }
 }
