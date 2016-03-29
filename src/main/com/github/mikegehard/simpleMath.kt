2ad753d20a0eed9e435da949ad2682180df9d5fe
Mike Gehard <mgehard@pivotal.io>
2016-03-29 16:23:06 -0600

    no negative numbers

--- src/main/com/github/mikegehard/simpleMath.kt
+++ src/main/com/github/mikegehard/simpleMath.kt
@@ -1,8 +1,8 @@
 package com.github.mikegehard
 
 fun main(args: Array<String>) {
-    val firstNumber: Int = getNumber("What is the first number? ")
-    val secondNumber: Int = getNumber("What is the second number? ")
+    val firstNumber: Int = getNumber("What is the first positive number? ")
+    val secondNumber: Int = getNumber("What is the second positive number? ")
 
     println("""
 $firstNumber + $secondNumber = ${firstNumber + secondNumber}
@@ -22,7 +22,7 @@ fun getNumber(prompt: String): Int {
             null
         }
 
-    } while (number == null)
+    } while (number == null || number < 0)
 
     return number
 }
